58992537628c0785e6e8bde62b1e78875bf608b4
marillkam <mlpmandorinka@gmail.com>
2025-03-02 23:51:18 +0300

    added new form delete event

---
+++ DeleteEventForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eventslab1
+{
+    public partial class DeleteEventForm : Form
+    {
+        private List<Event> events;
+        private ComboBox eventComboBox;
+        private Button deleteButton;
+        private Button cancelButton;
+
+        public DeleteEventForm(List<Event> events)
+        {
+            this.events = events;
+            this.Text = "Удалить событие";
+            this.Size = new System.Drawing.Size(300, 100);
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            eventComboBox = new ComboBox
+            {
+                Location = new System.Drawing.Point(10, 10),
+                Size = new System.Drawing.Size(260, 20)
+            };
+            foreach (var e in events)
+            {
+                eventComboBox.Items.Add(e.Name);
+            };
+
+            deleteButton = new Button
+            {
+                Text = "Удалить",
+                Location = new System.Drawing.Point(10, 40),
+                Size = new System.Drawing.Size(100, 25)
+            };
+            deleteButton.Click += (sender, e) =>
+            {
+                if (eventComboBox.SelectedIndex >= 0)
+                {
+                    events.RemoveAt(eventComboBox.SelectedIndex);
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            };
+
+            cancelButton = new Button
+            {
+                Text = "Отмена",
+                Location = new System.Drawing.Point(170, 40),
+                Size = new System.Drawing.Size(100, 25)
+            };
+            cancelButton.Click += (sender, e) =>
+            {
+                this.Close();
+            };
+
+            this.Controls.Add(eventComboBox);
+            this.Controls.Add(deleteButton);
+            this.Controls.Add(cancelButton);
+        }
+    }
+}
